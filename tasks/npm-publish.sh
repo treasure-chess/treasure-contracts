f444edcf6550617272aa9a0539858e9b23430575
patrick <blockchainbuddha@gmail.com>
2021-11-11 13:33:51 -0500

    add npmjs publishing

--- tasks/npm-publish.sh
+++ tasks/npm-publish.sh
@@ -6,10 +6,9 @@ PACKAGE_DIR="$1"
 TAG="$2"
 shift 2
 
-# Uncomment to begin publishing to npm
-# echo "Publishing ${PACKAGE_DIR} @${TAG} to NPMJS registry"
-# $D/npmrc-use-npmjs.sh > .npmrc
-# npm publish --tag ${TAG} ${PACKAGE_DIR} "$@"
+echo "Publishing ${PACKAGE_DIR} @${TAG} to NPMJS registry"
+$D/npmrc-use-npmjs.sh > .npmrc
+npm publish --tag ${TAG} ${PACKAGE_DIR} "$@"
 
 echo "Publishing ${PACKAGE_DIR} @${TAG} to Github Packages"
 $D/npmrc-use-github.sh > .npmrc
