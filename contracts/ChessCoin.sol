53ba9a9ab13055bf1924094197946d06ee194000
patrick <blockchainbuddha@gmail.com>
2022-01-26 15:17:09 -0500

    prettier again

--- contracts/ChessCoin.sol
+++ contracts/ChessCoin.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ChessCoin is ERC20 {
-  constructor(uint256 _amount) ERC20('ChessCoin', 'CHSC') {
-    _mint(msg.sender, _amount);
-  }
+    constructor(uint256 _amount) ERC20("ChessCoin", "CHSC") {
+        _mint(msg.sender, _amount);
+    }
 }
