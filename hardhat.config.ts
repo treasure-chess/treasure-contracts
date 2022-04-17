5c815db336b3bdee953e7cb482bce38a788c8bd0
jschiarizzi <jschiarizzi@gwu.edu>
2022-04-16 21:06:12 -0400

    pretty + dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,23 @@
-import { HardhatUserConfig, task } from 'hardhat/config'
-import { config as dotEnvConfig } from 'dotenv'
-dotEnvConfig()
-import 'hardhat-prettier'
-import '@typechain/hardhat'
-import 'solidity-coverage'
-import '@nomiclabs/hardhat-ethers'
-import '@openzeppelin/hardhat-upgrades'
+import { HardhatUserConfig, task } from "hardhat/config";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+import "hardhat-prettier";
+import "@typechain/hardhat";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+
+import "@nomiclabs/hardhat-etherscan";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (_args, hre) => {
-  const accounts = await hre.ethers.getSigners()
+task("accounts", "Prints the list of accounts", async (_args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address)
+    console.log(account.address);
   }
-})
+});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -25,7 +27,7 @@ task('accounts', 'Prints the list of accounts', async (_args, hre) => {
  */
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.2', // must be 0.8.2 due to ERC1967UpgradeUpgradeable.sol
+    version: "0.8.2", // must be 0.8.2 due to ERC1967UpgradeUpgradeable.sol
     settings: {
       optimizer: {
         enabled: true,
@@ -35,13 +37,13 @@ const config: HardhatUserConfig = {
   },
   networks: {
     localhost: {
-      url: 'http://127.0.0.1:8545',
-	  accounts: [process.env.PRIVATE_KEY || ""]
+      url: "http://127.0.0.1:8545",
+      accounts: [process.env.PRIVATE_KEY || ""],
     },
     hardhat: {
-    //   forking: {
-    //     url: 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY,
-    //   },
+      //   forking: {
+      //     url: 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY,
+      //   },
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
@@ -56,23 +58,23 @@ const config: HardhatUserConfig = {
       url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
       gasPrice: 8000000000,
-      timeout: 4000000
+      timeout: 4000000,
     },
-    rinkeby:{
+    rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
-    goerli:{
+    goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
-    arbitrum:{
+    arbitrum: {
       url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
-    }
+    },
   },
-}
+};
 
 //npx hardhat run --network ropsten deploy/001_deploy_contracts.ts
 
-export default config
+export default config;
